e8af94b0a1330de85e548a837d2aebeb3a14875f
Timothy <todongoonditi@gmail.com>
2023-03-21 12:44:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    var heading = document.getElementById('text');
+    heading.textContent = 'This is really cool!';
+  });
+  
